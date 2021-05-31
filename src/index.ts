8fe4813513e3a11a23a1b8086b7ec343855ccdbb
Firstname Lastname <denis.vybornyj@gmail.com>
2021-05-31 20:10:36 +0300

    fix: export all components

--- src/index.ts
+++ src/index.ts
@@ -5,44 +5,40 @@ export * from './components/Button'
 export * from './components/ButtonMenu'
 export * from './components/Card'
 export * from './components/Checkbox'
+export * from './components/ConnectionLoad'
 export * from './components/Dropdown'
 export * from './components/Flex'
 export * from './components/Heading'
 export * from './components/Image'
 export * from './components/Input'
 export * from './components/Layouts'
+export * from './components/Link'
+export * from './components/Overlay'
+export * from './components/Progress'
 export * from './components/Radio'
+export * from './components/Skeleton'
+export * from './components/SocialNetworks'
+export * from './components/Spinner'
 export * from './components/Svg'
+export * from './components/Table'
 export * from './components/Tag'
 export * from './components/Text'
-export * from './components/Link'
-export * from './components/Progress'
-export * from './components/Spinner'
-export * from './components/Skeleton'
-export * from './components/Toggle'
-export * from './components/Table'
-export * from './components/SocialNetworks'
 export * from './components/ToastPending'
-
+export * from './components/Toggle'
+// Config
+export * from './config'
 // Hooks
 export * from './hooks'
-
-// Widgets
-export * from './widgets/Modal'
-export * from './widgets/Menu'
-export * from './widgets/Toast'
-export * from './widgets/WalletModal'
-
 // Pages
 export * from './pages/Maintenance'
 export * from './pages/NotFound'
-
 // Theme
 export { default as ResetCSS } from './ResetCSS'
 export * from './theme'
-
 // Utils
 export * from './util'
-
-// Config
-export * from './config'
+export * from './widgets/Menu'
+// Widgets
+export * from './widgets/Modal'
+export * from './widgets/Toast'
+export * from './widgets/WalletModal'
